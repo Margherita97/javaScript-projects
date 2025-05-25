856fb1a993b150a5c4dedbeab6555a63fba7efc7
Margherita <145118725+Margherita97@users.noreply.github.com>
2025-05-25 10:40:09 +0200

    Add if statememt

--- love-calculator/myScript.js
+++ love-calculator/myScript.js
@@ -6,6 +6,14 @@ function loveCalculator () {
 
     const percentage = Math.floor((Math.random() * 100) + 1);
 
-    if (percentage 
+    if (percentage >= 80) {
+        return 'WOW! That\'s love!';
+    } else if (percentage >= 30) {
+        return 'You look good together :)';
+    } else {
+        return 'Better keep on looking...';
+    }
+
+    r
 
 }
