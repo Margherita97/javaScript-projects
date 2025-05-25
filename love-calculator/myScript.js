d47714fac671716d5f54e80d8b87c610ce398601
Margherita <145118725+Margherita97@users.noreply.github.com>
2025-05-25 10:37:44 +0200

    Create yourName, crushName and loveCalculator()

---
+++ love-calculator/myScript.js
@@ -0,0 +1,11 @@
+const yourName = prompt('What is your name?');
+
+const crushName = prompt('What si your crush\'s name?');
+
+function loveCalculator () {
+
+    const percentage = Math.floor((Math.random() * 100) + 1);
+
+    if (percentage 
+
+}
